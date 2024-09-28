3f30d13ee3c9f2502ac07ad228770b21fba16b86
Jkutney21 <108850690+Jkutney21@users.noreply.github.com>
2024-09-28 15:08:00 -0500

    signup
    .

--- front_end/our-front-end-react/src/App.jsx
+++ front_end/our-front-end-react/src/App.jsx
@@ -1,14 +1,15 @@
-import GameCards from './Components/GameCards';
-import NavBar from './Components/NavBar' 
+
 import AboutPage from './Pages/AboutPage';
 import AddItem from './Pages/AddItem';
 import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
 import Favorited from './Pages/Favorited';
 import Popular from './Pages/Popular';
 import LandingPage from './Pages/LandingPage';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter,Routes,Route } from "react-router-dom";
+
 function App() {
   return (
     <>
@@ -23,8 +24,7 @@ function App() {
         <Route path="/Popular" element={<Popular />} />
       </Routes>
     </BrowserRouter>
-    <LandingPage/>
-
+    
     </>
   )
 }
