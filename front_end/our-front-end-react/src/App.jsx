1f9d6d5addc03da54c9d894233b1017978d8a1b7
leejac1613 <leejac608@gmail.com>
2024-09-28 14:25:35 -0500

    Pushed Landing Page

--- front_end/our-front-end-react/src/App.jsx
+++ front_end/our-front-end-react/src/App.jsx
@@ -6,7 +6,7 @@ import SignIn from './Pages/SignIn';
 import Favorited from './Pages/Favorited';
 import Popular from './Pages/Popular';
 import LandingPage from './Components/LandingPage';
-
+import Theme from './Components/Theme';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 function App() {
@@ -24,7 +24,7 @@ function App() {
         <Route path="/Popular" element={<Popular />} />
       </Routes>
     </BrowserRouter>
-    <LandingPage/>
+    <Theme/>
 
     </>
   )
--- front_end/our-front-end-react/src/Components/LandingPage.jsx
+++ front_end/our-front-end-react/src/Components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import GameCards from '../Components/GameCards'
-import NavBar from '../Components/NavBar'
+import GameCards from './GameCards'
+import NavBar from './NavBar'
 function LandingPage() {
   return (
     <>
@@ -18,11 +18,17 @@ function LandingPage() {
             </p>
         </div>
     </div>
-    <div className = 'row'>
-        <GameCards/>
-        <GameCards/>
-        <GameCards/>
-        <GameCards/>
+    <div className = 'container-fluid'>
+      <div className='row justify-content-center'>
+        <div className = 'col-md-12'>
+          <div className ='row'>
+          <GameCards/>
+          <GameCards/>
+          <GameCards/>
+          <GameCards/>
+          </div>
+        </div>
+      </div>
     </div>
     </>
   )
