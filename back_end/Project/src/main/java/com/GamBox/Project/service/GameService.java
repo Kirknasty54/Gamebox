c85b2877b3ed220a2e200f8b42269d3e823fa80a
Jackson Kirkpatrick <93236436+Kirknasty54@users.noreply.github.com>
2024-09-29 06:11:16 -0500

    come up to date

---
+++ back_end/Project/src/main/java/com/GamBox/Project/service/GameService.java
@@ -0,0 +1,64 @@
+package com.GamBox.Project.service;
+
+import com.GamBox.Project.repository.GameInfoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.GamBox.Project.domain.GameInfo;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class GameService {
+  @Autowired
+  private GameInfoRepository gameInfoRepository;
+
+  public List<GameInfo> allGames() {
+    return gameInfoRepository.findAll();
+  }
+}
+
+/*
+ * import com.GamBox.Project.domain.UserInfo;
+ * import com.GamBox.Project.repository.UserInfoRepository;
+ * import lombok.AllArgsConstructor;
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.stereotype.Service;
+ * 
+ * import java.util.List;
+ * import java.util.Optional;
+ * 
+ * @Service
+ * 
+ * @AllArgsConstructor
+ * public class UserService {
+ * 
+ * @Autowired
+ * private UserInfoRepository userInfoRepository;
+ * 
+ * public UserInfo create(UserInfo user) {
+ * return userInfoRepository.save(user);
+ * }
+ * 
+ * public List<UserInfo> allUsers() {
+ * return userInfoRepository.findAll();
+ * }
+ * 
+ * public Optional<UserInfo> singleUser(Long uId) {
+ * return userInfoRepository.findById(uId);
+ * }
+ * 
+ * public Optional<UserInfo> auth(String userEmail, String password) {
+ * return userInfoRepository.findByEmailAndPassword(userEmail, password);
+ * }
+ * 
+ * public UserInfo save(UserInfo userInfo) {
+ * return userInfoRepository.save(userInfo);
+ * }
+ * 
+ * public Optional<UserInfo> check(String username) {
+ * return userInfoRepository.findByUserName(username);
+ * }
+ * }
+ */
