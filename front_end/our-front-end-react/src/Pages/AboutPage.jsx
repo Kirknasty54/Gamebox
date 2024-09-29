4c51e7f0b6cae21a3e65032856b69d6b5523f8cd
Jkutney21 <108850690+Jkutney21@users.noreply.github.com>
2024-09-29 00:40:17 -0500

    due

--- front_end/our-front-end-react/src/Pages/AboutPage.jsx
+++ front_end/our-front-end-react/src/Pages/AboutPage.jsx
@@ -1,4 +1,3 @@
-// AboutPage.js
 import React from 'react';
 import NavBar from '../Components/NavBar';
 import Container from 'react-bootstrap/Container';
@@ -7,75 +6,72 @@ import Col from 'react-bootstrap/Col';
 
 import './Aboutus.css';
 
-
 const AboutPage = () => {
   return (
     <>
       <NavBar />
       <h1 className='header'>About Us</h1>
       <Container>
-      <Row>
-        <Col>
-        <div className='box'>
-        <h3 className='title'>Collin Davis</h3>
-        <div className='content'> {/* Add this class for flexbox layout */}
-          <img title='about-image' src='collin.svg' className='about-image' alt='About Us'/>
-          <p className='p1'>
-              <a className="text-light" href="https://www.linkedin.com/in/collin-davis-8a862a226/">LinkedIn</a>
-          </p>
-        </div>
-      </div>
-        
-        </Col>
-        <Col>
-        <div className='box'>
-        <h3 className='title'>Jacob Lee</h3>
-        <div className='content'> {/* Add this class for flexbox layout */}
-          <img title='about-image' src='jacob1.svg' className='about-image' alt='About Us'/>
-          <p className='p1'>
-          <a className="text-light" href="www.linkedin.com/in/john-k-34349424a">LinkedIn</a>
-          </p>
-        </div>
-      </div>
-      </Col>
-      </Row>
-      <Row>
-        <Col>
-        <div className='box1'>
-        <h3 className='title1'>John Kutney</h3>
-        <div className='content1'> {/* Add this class for flexbox layout */}
-          <img title='about-image1' src='John.svg' className='about-image' alt='About Us'/>
-          <p className='p2'>
-          <a className="text-light" href="www.linkedin.com/in/john-k-34349424a">LinkedIn</a>
-          </p>
-        </div>
-      </div>
-        
-        </Col>
-        <Col>
-        <div className='box1'>
-        <h3 className='title1'>Noah Bowling</h3>
-        <div className='content1'> {/* Add this class for flexbox layout */}
-          <img title='about-image1' src='collin.svg' className='about-image' alt='About Us'/>
-          <p className='p2'>
-            We are dedicated to providing the best services to our customers. Our mission is to...
-          </p>
-        </div>
-      </div>
-      </Col>
-        <Col>
-        <div className='box1'>
-        <h3 className='title1'>Jackson Kirkpatrick</h3>
-        <div className='content1'> {/* Add this class for flexbox layout */}
-          <img title='about-image1' src='collin.svg' className='about-image' alt='About Us'/>
-          <p className='p2'>
-            We are dedicated to providing the best services to our customers. Our mission is to...
-          </p>
-        </div>
-      </div>
-        </Col>
-      </Row>
-    </Container>
+        <Row>
+          <Col>
+            <div className='box'>
+              <h3 className='title'>Collin Davis</h3>
+              <div className='content'>
+                <img title='about-image' src='collin.svg' className='about-image' alt='About Us'/>
+                <p className='p1'>
+                  <a className="text-light" href="https://www.linkedin.com/in/collin-davis-8a862a226/">LinkedIn</a>
+                </p>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className='box'>
+              <h3 className='title'>Jacob Lee</h3>
+              <div className='content'>
+                <img title='about-image' src='jacob1.svg' className='about-image' alt='About Us'/>
+                <p className='p1'>
+                  <a className="text-light" href="https://www.linkedin.com/in/john-k-34349424a">LinkedIn</a>
+                </p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className='box'>
+              <h3 className='title'>John Kutney</h3>
+              <div className='content'>
+                <img title='about-image' src='John.svg' className='about-image' alt='About Us'/>
+                <p className='p1'>
+                  <a className="text-light" href="https://www.linkedin.com/in/john-k-34349424a">LinkedIn</a>
+                </p>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className='box'>
+              <h3 className='title'>Noah Bowling</h3>
+              <div className='content'>
+                <img title='about-image' src='collin.svg' className='about-image' alt='About Us'/>
+                <p className='p1'>
+                  We are dedicated to providing the best services to our customers. Our mission is to...
+                </p>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className='box'>
+              <h3 className='title'>Jackson Kirkpatrick</h3>
+              <div className='content'>
+                <img title='about-image' src='collin.svg' className='about-image' alt='About Us'/>
+                <p className='p1'>
+                  We are dedicated to providing the best services to our customers. Our mission is to...
+                </p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
--- front_end/our-front-end-react/src/Pages/Profile.jsx
+++ front_end/our-front-end-react/src/Pages/Profile.jsx
@@ -1,40 +1,65 @@
-import React from 'react'
-import NavBar from '../Components/NavBarUser'
+import React from 'react';
+import NavBar from '../Components/NavBarUser';
+import './Profile.css';
 
+const likedGames = [
+    { id: 1, title: 'Game One', genre: 'Action' },
+    { id: 2, title: 'Game Two', genre: 'Adventure' },
+    { id: 3, title: 'Game Three', genre: 'Puzzle' },
+];
 
+const gameList = [
+    { id: 1, title: 'Game A', genre: 'RPG' },
+    { id: 2, title: 'Game B', genre: 'Strategy' },
+    { id: 3, title: 'Game C', genre: 'Shooter' },
+];
 
-function Profile(){
-  return (
-    <>
-    <NavBar/>
-    <div className = "container">
-      <div className ="row">
-        <div className = "col-md-3">
-          <h1>Username</h1>
-        </div>
-      </div>
-      <div className = "row justify-content-center">
-        <div className = "col-md-8 align-self-center mt-2">
-          <h4>My Favorite Games</h4>
-          <hr></hr>
-        </div>
-      </div>
-        <div className = "row justify-content-center">
-          <div className = "col-md-8 align-self-center mt-2">
-            <h4>My Recent Reviews</h4>
-            <hr></hr>
-        </div>
-        </div>
-              <div className = "row justify-content-center">
-          <div className = "col-md-8 align-self-center mt-2">
-            <h4>My Lists</h4>
-            <hr></hr>
-        </div>
-      </div>
-    </div>
+function Profile() {
+    return (
+        <>
+            <NavBar />
+            <div className="profile-container">
+                <div className="profile-header">
+                    <img src="./D1.png" alt="Profile" className="profile-picture" />
+                    <h1 className="profile-name">John Doe</h1>
+                </div>
+                <div className="profile-details">
+                    <p className="profile-email">Email: johndoe@example.com</p>
+                    <p className="profile-bio">
+                        Bio: A passionate gamer and tech enthusiast. Always looking to explore new adventures!
+                    </p>
+                </div>
+                
+                <div className="profile-games">
+                    <h2>Liked Games</h2>
+                    <ul>
+                        {likedGames.map(game => (
+                            <li key={game.id}>
+                                {game.title} - {game.genre}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-    </>
-  )
+                <div className="profile-games">
+                    <h2>Game List</h2>
+                    <ul>
+                        {gameList.map(game => (
+                            <li key={game.id}>
+                                {game.title} - {game.genre}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="profile-actions">
+                    <button className="edit-button">Edit Profile</button>
+                    <button className="logout-button">Logout</button>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default Profile
+export default Profile;
+
