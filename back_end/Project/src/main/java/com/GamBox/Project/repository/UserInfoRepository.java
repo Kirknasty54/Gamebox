c03f6a3be1b83ba7a87f9d5b64bec5f9a057ed99
noahBowling <nab08580@ucmo.edu>
2024-09-29 02:40:59 -0500

    uuyu

--- back_end/Project/src/main/java/com/GamBox/Project/repository/UserInfoRepository.java
+++ back_end/Project/src/main/java/com/GamBox/Project/repository/UserInfoRepository.java
@@ -5,20 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-<<<<<<< HEAD
 @Repository
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
   Optional<UserInfo> findByuId(Long uId);
-=======
-public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
-  UserInfo findByuId(Long userId);
->>>>>>> 4e339c4e8ee168f8dfd73db3cf1355259b88bbed
 
   Optional<UserInfo> findByUserNameAndPassword(String username, String password);
 
   Optional<UserInfo> findByUserName(String username);
-<<<<<<< HEAD
-=======
-
->>>>>>> 4e339c4e8ee168f8dfd73db3cf1355259b88bbed
 }
--- back_end/Project/src/main/java/com/GamBox/Project/service/UserService.java
+++ back_end/Project/src/main/java/com/GamBox/Project/service/UserService.java
@@ -1,30 +1,17 @@
 package com.GamBox.Project.service;
 
-<<<<<<< HEAD
 import com.GamBox.Project.domain.UserInfo;
 import com.GamBox.Project.repository.UserInfoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-=======
-import org.springframework.stereotype.Service;
-
-import lombok.AllArgsConstructor;
-import com.GamBox.Project.domain.UserInfo;
-import com.GamBox.Project.repository.UserInfoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> 4e339c4e8ee168f8dfd73db3cf1355259b88bbed
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class UserService {
-<<<<<<< HEAD
-=======
-
->>>>>>> 4e339c4e8ee168f8dfd73db3cf1355259b88bbed
   @Autowired
   private UserInfoRepository userInfoRepository;
 
@@ -36,13 +23,8 @@ public class UserService {
     return userInfoRepository.findAll();
   }
 
-<<<<<<< HEAD
   public Optional<UserInfo> singleUser(Long uId) {
     return userInfoRepository.findById(uId);
-=======
-  public Optional<UserInfo> singleUser(Long id) {
-    return userInfoRepository.findById(id);
->>>>>>> 4e339c4e8ee168f8dfd73db3cf1355259b88bbed
   }
 
   public Optional<UserInfo> auth(String username, String password) {
