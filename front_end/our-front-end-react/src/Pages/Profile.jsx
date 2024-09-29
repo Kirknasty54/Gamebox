3616d4876a08c27c7b48329a600ae593bca0fe28
Jkutney21 <108850690+Jkutney21@users.noreply.github.com>
2024-09-28 23:55:26 -0500

    lotes

--- front_end/our-front-end-react/src/Pages/Profile.jsx
+++ front_end/our-front-end-react/src/Pages/Profile.jsx
@@ -24,7 +24,7 @@ function Profile(){
             <h4>My Recent Reviews</h4>
             <hr></hr>
         </div>
-      </div>
+        </div>
               <div className = "row justify-content-center">
           <div className = "col-md-8 align-self-center mt-2">
             <h4>My Lists</h4>
