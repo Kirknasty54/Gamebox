54df8eeb8e2e7f9da4099e5db92a8194200e7411
Jkutney21 <108850690+Jkutney21@users.noreply.github.com>
2024-09-29 00:12:29 -0500

    push

--- front_end/our-front-end-react/src/Components/NavbarUser.jsx
+++ front_end/our-front-end-react/src/Components/NavbarUser.jsx
@@ -6,44 +6,55 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import './NavBarUser.css';
 
-
 function NavBarUser() {
+    const RandomNumber = Math.floor(Math.random() * 6) + 1;
     return (
-      <>
-      <Navbar expand="sm" bg="dark" data-bs-theme="light" className="bg-body-tertiary fs-6 shadow-lg py-2">
-      <Container fluid>
-          <Navbar.Brand href="/" className="d-flex align-items-center">
-              <img src="./logo.png" alt="GameBox Logo" className="navbar-logo" />
-              GameBox
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-              <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-                  <Nav.Link href="/">Home</Nav.Link>
-                  <Nav.Link href="/about">About</Nav.Link>
-                  <NavDropdown title="My List" id="navbarScrollingDropdown">
-                      <NavDropdown.Item href="/Popular">Popular</NavDropdown.Item>
-                      <NavDropdown.Item href="/Favorited">Favorited</NavDropdown.Item>
-                  </NavDropdown>
-              </Nav>
-              <Form className="d-flex">
-                  <Form.Control
-                      type="search"
-                      placeholder="Search"
-                      className="me-2"
-                      aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-              </Form>
-              <Nav.Link href="/SignIn" className="ms-3 text-secondary">
-                  
-              </Nav.Link>
-          </Navbar.Collapse>
-      </Container>
-  </Navbar>
-  </>
-       
-  );
+        <>
+            <Navbar expand="sm" bg="dark" data-bs-theme="light" className="bg-body-tertiary fs-6 shadow-lg py-2">
+                <Container fluid>
+                    <Navbar.Brand href="/" className="d-flex align-items-center">
+                        <img src="./logo.png" alt="GameBox Logo" className="navbar-logo" />
+                        GameBox
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/about">About</Nav.Link>
+                            <NavDropdown title="My List" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="/Popular">Popular</NavDropdown.Item>
+                                <NavDropdown.Item href="/Favorited">Favorited</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                        {/* Profile Dropdown */}
+                        <NavDropdown title="Profile" id="profileDropdown" className="ms-3" drop="down" align="end">
+                            <NavDropdown.Item href="/profile">View Profile</NavDropdown.Item>
+                            <NavDropdown.Item href="/settings">Settings</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                        </NavDropdown>
+
+                        {/* User Image */}
+                        <img 
+                            src="./user-avatar.png" // replace with your image path
+                            alt="User Avatar"
+                            className="user-avatar ms-3"
+                        />
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
 
 export default NavBarUser;
+
