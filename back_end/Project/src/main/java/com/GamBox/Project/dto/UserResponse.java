1bba6680a16d40c37d21e9023efe3344dad6fa3a
Jackson Kirkpatrick <93236436+Kirknasty54@users.noreply.github.com>
2024-10-07 12:27:58 -0500

    fixed java runtime error

---
+++ back_end/Project/src/main/java/com/GamBox/Project/dto/UserResponse.java
@@ -0,0 +1,12 @@
+package com.GamBox.Project.dto;
+
+import com.GamBox.Project.domain.UserInfo;
+
+public class UserResponse {
+  private Long UId;
+
+  public UserResponse(UserInfo userInfo) {
+
+  }
+
+}
