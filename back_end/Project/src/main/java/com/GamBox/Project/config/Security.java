c25aad3a15e4b9642a1c9dda42de1dc310b1c770
Jackson Kirkpatrick <93236436+Kirknasty54@users.noreply.github.com>
2024-10-08 15:25:20 -0500

    allows all users to access login and register endpoints, everything else will require authentication via Security.java

--- back_end/Project/src/main/java/com/GamBox/Project/config/Security.java
+++ back_end/Project/src/main/java/com/GamBox/Project/config/Security.java
@@ -20,7 +20,8 @@ public class Security {
     })
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/login").permitAll() // Allow access to login endpoint
-            .requestMatchers("/v1/user/**").permitAll() // Allow access to user-related endpoints
+            .requestMatchers("/v1/users/auth").permitAll() // Allow access to user-related endpoints
+            .requestMatchers("/v1/users/register").permitAll() // Allow access to user-related endpoints
             .requestMatchers("/v1/games/**").permitAll() // Allow access to all game-related endpoints
             .anyRequest().authenticated() // Require authentication for all other requests
         )
