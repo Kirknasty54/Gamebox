74fa02feddd9bf08d8daae72bea81f57c5b9abe9
Jackson Kirkpatrick <93236436+Kirknasty54@users.noreply.github.com>
2024-11-04 23:00:04 -0600

    last ditch attempt, made so that all user endpoint requests are authorized

--- back_end/Project/src/main/java/com/GamBox/Project/config/Security.java
+++ back_end/Project/src/main/java/com/GamBox/Project/config/Security.java
@@ -20,8 +20,9 @@ public class Security {
     })
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/login").permitAll() // Allow access to login endpoint
-            .requestMatchers("/v1/users/auth").permitAll() // Allow access to user-related endpoints
-            .requestMatchers("/v1/users/register").permitAll() // Allow access to user-related endpoints
+            .requestMatchers("/v1/users/**").permitAll() // Allow access to user-related endpoints
+            // .requestMatchers("/v1/users/register").permitAll() // Allow access to
+            // user-related endpoints
             .requestMatchers("/v1/games/**").permitAll() // Allow access to all game-related endpoints
             .anyRequest().authenticated() // Require authentication for all other requests
         )
