601f280905f654bf5f4375e32e39680463ac22e0
Jackson Kirkpatrick <93236436+Kirknasty54@users.noreply.github.com>
2024-11-08 10:33:32 -0600

    added local host to allowed connectors in WebConfig. doubt this will work like 90% of the shit i do

--- back_end/Project/src/main/java/com/GamBox/Project/config/WebConfig.java
+++ back_end/Project/src/main/java/com/GamBox/Project/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("https://gameboxusa.com", "https://localhost:8090", "http://153.91.239.13:5173")
+        .allowedOrigins("https://gameboxusa.com", "https://localhost:8090", "http://153.91.239.13:5173",
+            "http://localhost:5173")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
